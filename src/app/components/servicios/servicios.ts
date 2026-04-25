412eef594d99d25bd3b36bd1aaa795948f2e13e1
AlboLock <albertolucchetti@gmail.com>
2026-04-25 20:00:44 +0200

    Added blog dinamico con posibilidad de eliminar post

--- src/app/components/servicios/servicios.ts
+++ src/app/components/servicios/servicios.ts
@@ -1,10 +1,12 @@
 import { NgClass, NgFor, NgIf } from '@angular/common';
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BlogCard } from '../blog_card/blog_card';
 
 @Component({
   selector: 'app-servicios',
   standalone: true,
-  imports: [NgClass, NgIf, NgFor],
+  imports: [NgClass, NgIf, NgFor, ReactiveFormsModule, BlogCard],
   templateUrl: './servicios.html',
   styleUrl: './servicios.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -29,5 +31,39 @@ export class ServiciosComponent {
 
   selectImage(image: any) {
     this.selectedImage = image;
+  }
+  fb = inject(FormBuilder);
+
+  posts = [
+    {
+      id: 1,
+      title: 'Primer post',
+      description: 'Este es un ejemplo de publicación'
+    }
+  ];
+
+  form = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', [Validators.required, Validators.minLength(10)]]
+  });
+
+  addPost() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const newPost = {
+      id: Date.now(),
+      title: this.form.value.title!,
+      description: this.form.value.description!
+    };
+
+    this.posts.unshift(newPost);
+    this.form.reset();
+  }
+
+  deletePost(id: number) {
+    this.posts = this.posts.filter(p => p.id !== id);
   }
 }
